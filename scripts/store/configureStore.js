4af31497c35ed271088b01b2af2a5fe3898aa314
fury93 <www.fanreala@gmail.com>
2016-08-03 13:44:15 +0300

    Added DevTool extension

--- scripts/store/configureStore.js
+++ scripts/store/configureStore.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
-
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, compose(
+      applyMiddleware(thunkMiddleware),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
 
   return store;
 }
